2f6f2392d1a74b7136fa17809121c00b4006645a
teakyungg <kak0502@naver.com>
2025-04-05 15:19:49 +0900

    bug/side-bar

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 let side = document.getElementById("side-bar");
 let side_max_height = document.body.scrollHeight * (30/100); /* 전체 높이의 30% */
+let first_scroll = false;
 
 /* side-bar */
 window.addEventListener('scroll',function(){
+
+    if(!first_scroll)
+    {
+        first_scroll = true;
+        return;
+    }
     
-    if(this.window.scrollY > side_max_height){ 
+    if(window.scrollY > side_max_height){ 
         side.style.opacity = '0';
         side.style.pointerEvents = 'none';
         side.style.transition = 'all 0.5s ease';
