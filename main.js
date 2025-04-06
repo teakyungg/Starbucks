fbe942be23d2ea44a1a0029e8ccd569955bc2cb9
teakyungg <skeadee@users.noreply.github.com>
2025-04-06 17:34:34 +0900

    feat/inner-img

--- main.js
+++ main.js
@@ -24,3 +24,27 @@ window.addEventListener('scroll',function(){
     }
 
 })
+
+
+/* inner img */
+let fade = document.getElementsByClassName("fade");
+
+for(let i=1;i<=fade.length;i++){
+    
+    fade[i-1].style.opacity = 0;
+    let opacity = 0;
+
+    setTimeout(() => {
+
+        for(let j=0;j<1000;j++){
+
+            setTimeout(() => {
+                opacity += 0.01;
+                fade[i-1].style.opacity = opacity;
+            },j*10);
+            
+        }
+    
+    },1000 * i)
+}
+
